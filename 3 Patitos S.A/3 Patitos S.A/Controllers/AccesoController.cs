15ddcb137c428d1603d5febe35793e518b6b6258
ALVARO JOSE BENAVIDES MADRIGAL <abenavidesm@edu.uia.ac.cr>
2023-03-25 09:47:22 -0600

    login

---
+++ 3 Patitos S.A/3 Patitos S.A/Controllers/AccesoController.cs
@@ -0,0 +1,57 @@
+﻿using _3_Patitos_S.A.Data;
+using _3_Patitos_S.A.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net;
+
+namespace _3_Patitos_S.A.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Acceso
+        public ActionResult Enter(string Usuario, string Contrasena)
+        {
+
+            try
+            {
+                using (Db_Context db = new Db_Context())
+
+                {
+
+                    var lst = from d in db.Persona
+                              where d.Correo == Usuario && d.Contrasena == Contrasena
+                              select d;
+                    if (lst.Count() > 0)
+                    {
+
+                        Usuario oUser = lst.First();
+                        Session["User"] = oUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario invalido");
+                    }
+
+
+                }
+            }
+
+            
+            catch (Exception ex)
+            {
+                return Content("Error" + ex.Message);
+            }
+
+
+        }
+    }
+}
